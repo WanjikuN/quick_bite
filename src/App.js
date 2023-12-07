5dd6166c55c1a746d479efa1542d8a7520f93230
WanjikuN <cikunjoroge4@gmail.com>
2023-12-07 13:18:14 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import LandingPage from './components/landingpage';
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <LandingPage />
+    </>
+  );
+}
+
+export default App;
