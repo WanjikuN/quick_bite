9be754c0baffa47ec7eef8fa2127312304eb89bf
WanjikuN <cikunjoroge4@gmail.com>
2023-12-10 15:00:48 +0300

    feat: add routing to the authentication component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import LandingPage from './components/landingpage';
 import Navbar from './components/navbar';
-
+import { Routes, Route } from 'react-router-dom';
+import Authentication from './components/Authentication';
+import { useState } from 'react';
 function App() {
+  const [type, setType] = useState(false);
+  const setTypeCallback = (newType) => {
+    setType(newType); // Update the type state
+  };
   return (
     <div id="app-container">
-    <Navbar />
-    <LandingPage />
+      <Navbar setType={setTypeCallback}/>
+     <Routes >
+
+     <Route exact path="/" element={<LandingPage />} />
+ 
+    <Route path="/login" element={<Authentication setType={setTypeCallback} type={type}/>} />
+    </Routes>
+    
+    
     </div>
   );
 }
