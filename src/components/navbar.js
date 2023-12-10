8983a0fc74ec56bb99fd384cafc883d99e9a7ea4
WanjikuN <cikunjoroge4@gmail.com>
2023-12-10 08:32:37 +0300

    feat: add navbar component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,8 @@ export default function Navbar(){
     return(
         <div id="navbar">
             <div className="logo">
-                <h2>QUICK BITE</h2>
+                <h2>QUICK</h2>
+                    <h2>BITE</h2> 
             </div>
             <div className="nav-buttons">
                 <button>Sign in</button>
